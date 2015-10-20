b3d7325b9559f1bdcbddc0802406c8b09d22acf9
e.pol <eugene.poletski@gmail.com>
2015-10-20 16:43:34 +0300

    Created todo.js

---
+++ js/models/todo.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var app = app || {};
+
+app.Todo = Backbone.Model.extend({
+    defaults: {
+        title: '',
+        completed: false
+    },
+    toggle: function () {
+        this.save({
+            completed: !this.get('completed')
+        });
+    }
+});
